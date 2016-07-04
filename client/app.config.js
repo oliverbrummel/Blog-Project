3950cbdab639b18e02d333fd8d90161cc549dc18
oliverbrummel <oliverbrummel1@gmail.com>
2016-07-04 17:25:42 -0500

    zipcode api and google map search functionality set up

--- client/app.config.js
+++ client/app.config.js
@@ -1,16 +1,30 @@
 angular.module('openApp', [])
   .controller('MainController', ['$http', function($http){
+    var vm = this;
+    vm.latitude;
+    vm.longitude;
 
-      console.log('Where does this show??');
+    vm.enterZip = function(){
+      $http.get('/zipcodeApi/'+ vm.zipcode)
+        .then(function(response){
+          vm.latitude = response.data.lat;
+          vm.longitude = response.data.lng;
+          console.log('vm.latitude = ',vm.latitude);
+          console.log('vm.longitude = ',vm.longitude);
+          initialize();
+        })
+    }
+
 
       function initialize(){
           var mapProp = {
-            center: new google.maps.LatLng(51.508742, -0.120850),
-            zoom: 7,
+            // center: new google.maps.LatLng(44.9778, -93.2650),
+            center: new google.maps.LatLng(vm.latitude, vm.longitude),
+            zoom: 14,
             mapTypeId: google.maps.MapTypeId.ROADMAP
           };
           var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
       }
-      google.maps.event.addDomListener(window, 'load', initialize);
+      // google.maps.event.addDomListener(window, 'load', initialize);
 
   }]);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,23 @@
 var router = require('express').Router();
 var path = require('path');
+var requestMod = require('request');
 
 router.get('/', function(request, response){
   response.sendFile(path.join(__dirname, '../public/views/index.html'));
+});
+
+router.get('/zipcodeApi/:zipcode', function(request, response){
+  var zip_code = request.params.zipcode;
+  console.log(request.params);
+  console.log(zip_code);
+  requestMod('https://www.zipcodeapi.com/rest/1xtzhHL4LuwQE7vwgVvurgzGRz59ONTQlmqC4bHPFeFjlbPB6l6xBGetgjD57wVs/info.json/'+ zip_code + '/degrees', function(err, res, body){
+    if(err){
+      response.sendStatus(500);
+    } else {
+      response.send(body);
+      console.log('body:', body);
+    }
+  });
 });
 
 
